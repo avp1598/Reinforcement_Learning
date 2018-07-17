237e07263b5421f9c7e33106ec2114bb10ceba08
avp1598 <nakulparmar15@gmail.com>
2018-07-17 22:47:08 +0530

    Add files via upload

---
+++ maze.java
@@ -0,0 +1,143 @@
+/*
+OOP assignment 3
+ */
+package maze_solve;
+
+/**
+ *
+ * @author aditya veer parmar ajay raghavan venkatesh prabhu
+ *
+ */
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class maze extends JFrame {
+
+    /**
+     * Conventions:
+     *
+     * maze[row][col]
+     *
+     * Values: 0 = not-visited node 1 = wall (blocked) 8 = agent 9 = target node
+     *
+     * borders must be filled with "1" to void ArrayIndexOutOfBounds exception.
+     */
+    private int [][] grid = 
+        { {1,1,1,1,1,1,1,1,1,1},
+          {1,8,0,0,0,0,0,0,0,1},
+          {1,0,0,1,0,1,0,1,1,1},
+          {1,0,0,1,0,0,0,0,0,1},
+          {1,0,0,1,1,1,1,1,0,1},
+          {1,0,0,1,0,0,0,0,0,1},
+          {1,0,0,1,0,0,1,0,1,1},
+          {1,0,0,1,0,0,0,0,0,1},
+          {1,0,0,0,0,0,0,0,9,1},
+          {1,1,1,1,1,1,1,1,1,1}
+        };
+    /*private final int[][] grid
+            = {{1, 1, 1, 1, 1},
+            {1, 8, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 9, 1},
+            {1, 1, 1, 1, 1}
+            };*/
+    q_learning agent = new q_learning(grid, grid.length);
+    private final int[][] ogrid = agent.copy(grid);
+
+    public maze() {
+
+        setTitle("Maze Solving using q_learning");
+        setSize(400, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.translate(50, 50);
+        int[][] grid1 = agent.copy(ogrid);
+        int x[][] = agent.copy(ogrid);
+        for (int i = 0; i < agent.num_ep; i++) {
+            agent.grid = agent.copy(x);
+            grid1 = agent.copy(ogrid);
+            int step=0;
+            while (true) {
+                //try{
+                
+                for (int row = 0; row < grid1.length; row++) {
+                    for (int col = 0; col < grid1[0].length; col++) {
+                        Color color;
+                        switch (grid1[row][col]) {
+                            case 1:
+                                color = Color.BLACK;
+                                break;
+                            case 9:
+                                color = Color.GREEN;
+                                break;
+                            default:
+                                color = Color.WHITE;
+                        }
+                        g.setColor(color);
+                        g.fillRect(30 * col, 30 * row, 30, 30);
+                        g.setColor(Color.BLACK);
+                        g.drawRect(30 * col, 30 * row, 30, 30);
+                    }
+                }
+                for (int row = 0; row < grid1.length; row++) {
+                    for (int col = 0; col < grid1[0].length; col++) {
+                        Color color;
+                        if (grid1[col][row] == 8) {
+                            g.setColor(Color.cyan);
+                            g.fillOval(row * 30, col * 30, 30, 30);
+                        }
+                    }
+                }
+                
+                //}
+                //catch(Exception e){//agent.grid=agent.copy(ogrid);}
+                if(i>500){
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(maze.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                }
+                grid1 = agent.brain(i);
+                /*for (int row = 0; row < grid1.length; row++) {
+                    System.out.println();
+                    for (int col = 0; col < grid1[0].length; col++){
+                        System.out.print(grid1[row][col]+"\t");
+                    }
+                    }*/
+                //System.out.println();
+                //agent.grid = agent.copy(grid1);
+                if (grid1 == null) { 
+                    break;
+                }
+                //removeAll();
+                //validate();
+                //repaint();
+                System.out.println("step:"+step);
+                step++;
+            }
+            System.out.println("EPISODE NO:"+i);
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                maze view = new maze();
+                view.setVisible(true);
+            }
+        });
+    }
+
+}
---
+++ q_learning.java
@@ -0,0 +1,193 @@
+/*
+ OOP assignment 3
+ */
+package maze_solve;
+
+/**
+ *
+ * @author Aditya veer Parmar Ajay Raghavan Venkatesh Prabhu
+ */
+import java.util.Arrays;
+import java.util.Random;
+
+public class q_learning {
+
+    int grid[][] = new int[10][10];
+    int size;
+    double lr = 0.7, gamma = 0.95;
+    int num_ep = 5000;
+    double q_table[][] = new double[100][10];
+
+    q_learning(int[][] grid, int size) {
+        this.grid = grid;
+        this.size = size;
+        for (int i = 0; i < size * size; i++) {
+            for (int j = 0; j < 4; j++) {
+                q_table[i][j] = 0;
+            }
+        }
+        //o_grid = copy(grid);
+    }
+
+    int state_finder(int[] l) {
+        int temp = -1;
+        int state = -1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                temp++;
+                if (i == l[0] && j == l[1]) {
+                    state = temp;
+                }
+            }
+        }
+        return state;
+    }
+
+    int[] find_pos() {
+        int pos[] = new int[2];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == 8) {
+                    pos[0] = i;
+                    pos[1] = j;
+                    return pos;
+                }
+            }
+        }
+        return null;
+    }
+
+    void take_action(int action) {
+        int[] pos = find_pos();
+        int i = pos[0];
+        int j = pos[1];
+        grid[i][j] = 0;
+        switch (action) {
+            case 0:
+                if (grid[i][j + 1] != 1) {
+                    grid[i][j + 1] = 8;
+                } else {
+                    grid[i][j] = 8;
+                }
+                break;
+            case 1:
+                if (grid[i - 1][j] != 1) {
+                    grid[i - 1][j] = 8;
+                } else {
+                    grid[i][j] = 8;
+                }
+                break;
+            case 2:
+                if (grid[i][j - 1] != 1) {
+                    grid[i][j - 1] = 8;
+                } else {
+                    grid[i][j] = 8;
+                }
+                break;
+            case 3:
+                if (grid[i + 1][j] != 1) {
+                    grid[i + 1][j] = 8;
+                } else {
+                    grid[i][j] = 8;
+                }
+                break;
+        }
+    }
+
+    double max(double[] arr) {
+        double max = -9999;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    int[][] brain(int num) {
+        int goal = 0;
+        int[] l = new int[2];
+        int action;
+        double n = (double) num;
+        Random a = new Random();
+        int s = state_finder(find_pos()), s1;
+        double reward;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == 9) {
+                    l[0] = i;
+                    l[1] = j;
+                    goal = state_finder(l);
+                }
+            }
+        }
+
+        while (true) {
+            int max_i = 1;
+            double max_q = -99999;
+            if (a.nextDouble() > 20 / (n + 1)) {
+                for (int j = 0; j < 4; j++) {
+                    if (q_table[s][j] > max_q) {
+                        max_q = q_table[s][j];
+                        max_i = j;
+                    }
+                }
+                action = max_i;
+            } else action = a.nextInt(4);
+            take_action(action);
+            s1 = state_finder(find_pos());
+            if (s1 == goal) {
+                reward = 2;
+            } else {
+                reward = -0.1;
+            }
+            q_table[s][action] = q_table[s][action] + lr * (reward + gamma * max(q_table[s1]) - q_table[s][action]);
+            s = s1;
+            if (s == goal) {
+                break;
+            }
+            return grid;
+        }
+        return null;
+    }
+
+    StringBuffer path() {
+        int[] pos = new int[2];
+        int goal = -1;
+        StringBuffer path = new StringBuffer();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (grid[i][j] == 9) {
+                    pos[0] = i;
+                    pos[1] = j;
+                    goal = state_finder(pos);
+                }
+            }
+        }
+        int s = state_finder(find_pos());
+        while (true) {
+            int max_i = 1;
+            double max_q = -99999;
+            for (int j = 0; j < 4; j++) {
+                if (q_table[s][j] > max_q) {
+                    max_i = j;
+                }
+            }
+            path.append(max_i);
+            take_action(max_i);
+            s = state_finder(find_pos());
+            if (s == goal) {
+                break;
+            }
+        }
+        return path;
+    }
+
+    public int[][] copy(int[][] input) {
+        int[][] target = new int[input.length][];
+        for (int i = 0; i < input.length; i++) {
+            target[i] = Arrays.copyOf(input[i], input[i].length);
+        }
+        return target;
+    }
+}
